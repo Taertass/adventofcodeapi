5dd97190938fa4feea6c5a35c5c3d72dc7a27dfb
Bartal Clementsen <bac@formula.fo>
2019-11-30 19:05:36 +0000

    Added missing files

---
+++ AdventOfCodeApi/Controllers/AdventOfCodeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdventOfCodeApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AdventOfCodeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdventOfCodeController : ControllerBase
+    {
+        private readonly IAdventOfCodeService _adventOfCodeService;
+
+        public AdventOfCodeController(IAdventOfCodeService adventOfCodeService)
+        {
+            _adventOfCodeService = adventOfCodeService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.AocMember>>> Get()
+        {
+            return await _adventOfCodeService.GetAsync();
+        }
+    }
+}
